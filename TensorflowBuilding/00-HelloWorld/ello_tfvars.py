b3ee2b8d410a04d061d8815af865ae0c84ccba19
Trevor Knott <tknott95@hotmail.com>
2018-12-02 04:11:32 -0700

    UnderTheHood - MNIST Visuals

--- TensorflowBuilding/00-HelloWorld/ello_tfvars.py
+++ TensorflowBuilding/00-HelloWorld/ello_tfvars.py
@@ -6,3 +6,6 @@ hw = h + w
 # This way is so we may check the Tensor90 whenever
 # Calling Vars normal returns: !Hola AmigBroos# Calling TF way returns: Tensor("add:0", shape=(), dtype=string)
 print(hw)
+
+ans = sess.run(hw)
+
