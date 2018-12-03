8ff1315da4676ba465d3cf68a294b9e9f76a1e25
Trevor Knott <tknott95@hotmail.com>
2018-12-03 10:36:37 -0700

    LAZY-0404 _Lazy gad() Commit

--- TensorflowBuilding/04-FlowingTensors/TensorArraysAndShapes/dimensional_arrays_visualize.py
+++ TensorflowBuilding/04-FlowingTensors/TensorArraysAndShapes/dimensional_arrays_visualize.py
@@ -6,3 +6,14 @@ ex = _tf.constant([
     [4,5,6]
     ])
 print('\n is a 2x2 matrix \n List Input: {}'.format(ex.get_shape()))
+
+ex = _tf.constant(_np.array([
+    [
+        [1,2,3],[4,5,6]
+    ],
+    [
+        [3,1,4],[1,2,4]
+    ]
+    ]))
+
+print('\n 3D NumPy 2x2x3array input: {}'.format(ex.get_shape()))
