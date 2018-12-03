b5fe9da5204a4643fab1b985e546ba2bf3d3143d
Trevor Knott <tknott95@hotmail.com>
2018-12-02 19:19:04 -0700

    UnderTheHood - Graphs w/ Sess"

--- TensorflowBuilding/03-GraphCreation/constructGraph.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-print(tf.get_default_graph())
-
-g = tf.Graph()
-print(g)
-
-
---
+++ TensorflowBuilding/03-GraphCreation/instantiateGraph.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+print(tf.get_default_graph())
+
+g = tf.Graph()
+print(g)
+
+
+print("\n")
+g2 = tf.Graph()
+a = tf.constant(5)
+
+print(a.graph is g2)
+print(a.graph is tf.get_default_graph())
+
+print("\n")
+g3 = tf.get_default_graph()
+g4 = tf.Graph() # reused to mentally see for these basic examples #sloppyForLearning
+
+print(g3 is tf.get_default_graph())
+
+with g4.as_default():
+    print(g3 is tf.get_default_graph())
+
+print(g3 is tf.get_default_graph())
+
+print("\n____FETCHES____\n")
+
+
+z = tf.constant(4)
+y = tf.constant(17)
+x = tf.constant(33)
+
+v = tf.multiply(z,x)
+m = tf.add(y,z)
+n = tf.subtract(x,y)
+
+with tf.Session() as sess:
+    fetches = [z,y,x,v,m,n]
+    outs = sess.run(fetches)
+
+print("outs = {}".format(outs))
+print(type(outs[0]))
+
