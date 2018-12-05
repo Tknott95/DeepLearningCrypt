2e3a550c4c5a92063a96aa9640e37a4a5296cc38
Trevor Knott <tknott95@hotmail.com>
2018-12-05 03:33:41 -0700

    LAZY-0404 _Lazy gad() Commit

--- TensorflowBuilding/08-ConvolutionalNeuralNetworks/HandwrittenMNIST-CNN/main.py
+++ TensorflowBuilding/08-ConvolutionalNeuralNetworks/HandwrittenMNIST-CNN/main.py
@@ -32,6 +32,22 @@ y_conv = full_layer(fulll_drop, 10)
 cross_entropy = _tf.reduce_mean(_tf.nn.softmax_cross_entropy_with_logits(logits=y_conv, labels=y_))
 train_step = _tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 correct_prediction = _tf.equal(_tf.argmax(y_conv, 1), _tf.argmax(y_, 1))
-accuracy = _tf.reduce_mean(_tf.cast(correct_predicton, tf.float32))
+accuracy = _tf.reduce_mean(_tf.cast(correct_predicton, _tf.float32))
 
+with _tf.Sesson as sess:
+    sess.run(_tf.global_variables_initializer())
+    
+    for i in range(STEPS):
+        batch = mnist.train.next_batch(MINIBATCH_SIZE)
 
+        if i % 100 == 0:
+            train_accuracy = sess.run(accuracy, feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+            print("step {}, training accuracy {}".format(i, train_accuracy))
+
+        sess.run(train_step, feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+
+    X = mnist.test.images.reshape(10, 1000, 784)
+    Y = mnist.test.labels.reshape(10, 1000, 10)
+    test_accuracy = np.mean([sess.run(accuracy, feed_dict={x: X[i], y_: Y[i], keep_prob: 1.0}) for i in range(10)])
+
+print("test_accuracy: {}".format(test_accuracy))
